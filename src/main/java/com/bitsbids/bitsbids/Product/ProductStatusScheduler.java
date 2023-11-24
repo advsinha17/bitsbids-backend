42774ffedef143f08384983f91a56ced57484e54
Advik Sinha <advsinha112@gmail.com>
2023-11-25 01:43:01 +0530

    add scheduler to periodically check for products with elapsed bid times

---
+++ src/main/java/com/bitsbids/bitsbids/Product/ProductStatusScheduler.java
@@ -0,0 +1,34 @@
+package com.bitsbids.bitsbids.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ProductStatusScheduler {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Scheduled(fixedRate = 60000)
+    public void updateProductStatuses() {
+        LocalDateTime now = LocalDateTime.now();
+        List<Product> allProducts = productService.getActiveProducts();
+
+        for (Product product : allProducts) {
+            if (product.getBidClosingTime().isBefore(now)) {
+                if (product.getLatestBidAmount() == null) {
+                    product.setProductStatus(Product.ProductStatus.UNSOLD);
+                } else {
+                    product.setProductStatus(Product.ProductStatus.SOLD);
+                }
+                productRepository.save(product);
+            }
+        }
+    }
+}
